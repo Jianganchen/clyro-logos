b1ef36063aa0c64420f732d732ca922f28c444a1
Jianganchen <jianganchen@ufl.edu>
2025-04-08 18:41:56 -0400

    Wired up database with Convex, set up webhook with clerk

---
+++ convex/users.ts
@@ -0,0 +1,57 @@
+import { v, Validator } from "convex/values";
+import { UserJSON } from "@clerk/backend";
+import { internalMutation, query, QueryCtx } from "./_generated/server";
+
+export const getUsers = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("users").collect();
+  },
+});
+
+export const upsertFromClerk = internalMutation({
+  args: { data: v.any() as Validator<UserJSON> },
+  async handler(ctx, { data }) {
+    const userAttributes = {
+      email: data.email_addresses[0].email_address,
+      clerkUserId: data.id,
+      username: `${data.first_name} ${data.last_name}`,
+    };
+
+    const user = await userByClerkUserId(ctx, data.id);
+
+    if (user === null) {
+      await ctx.db.insert("users", userAttributes);
+    } else {
+      await ctx.db.patch(user._id, userAttributes);
+    }
+  },
+});
+
+export const deleteFromClerk = internalMutation({
+  args: { clerkUserId: v.string() },
+  async handler(ctx, { clerkUserId }) {
+    const user = await userByClerkUserId(ctx, clerkUserId);
+
+    if (user !== null) {
+      await ctx.db.delete(user._id);
+    } else {
+      console.warn(
+        `Can't delete user, there is none for Clerk user ID: ${clerkUserId}`
+      );
+    }
+  },
+});
+
+export async function getCurrentUser(ctx: QueryCtx) {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) return null;
+  return await userByClerkUserId(ctx, identity.subject);
+}
+
+async function userByClerkUserId(ctx: QueryCtx, clerkUserId: string) {
+  return await ctx.db
+    .query("users")
+    .withIndex("byClerkUserId", (q) => q.eq("clerkUserId", clerkUserId))
+    .unique();
+}
