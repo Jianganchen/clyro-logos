5f6a08a37466277011467a07603c5326f487e730
Jianganchen <jianganchen@ufl.edu>
2025-04-23 20:49:25 -0400

    This has to fix it

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,17 +1,17 @@
-export default function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     checkoutId?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
+  const checkoutId = searchParams?.checkoutId || null;
   // Checkout has been confirmed
   // Now, make sure to capture the Checkout.updated webhook event to update the order status in your system
 
   return (
     <div>
       <h1>Thank you! Your checkout is now being processed.</h1>
-      <p>checkoutId: {searchParams?.checkoutId}</p>
+      <p>checkoutId: {checkoutId}</p>
     </div>
   );
 }
